f3b471ac6cfe8fcdd6e97f63ecd5d8cfdbd2240b
dabuchhe <dabuchhe@student.42lyon.fr>
2026-02-11 20:34:40 +0100

    FIX: had space between arguments

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,22 +6,23 @@
 /*   By: dabuchhe <dabuchhe@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 21:25:12 by dabuchhe          #+#    #+#             */
-/*   Updated: 2026/02/04 21:33:07 by dabuchhe         ###   ########lyon.fr   */
+/*   Updated: 2026/02/11 19:34:13 by dabuchhe         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
 #include <string.h>
 
 int	main(int ac, char **av) {
 	if (ac < 2) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
 	for (int i = 1; i < ac; i++) {
 		for (size_t j = 0; j < strlen(av[i]); j++)
 			std::cout <<  (char)std::toupper(av[i][j]);
+		if (i != ac - 1)
+			std::cout << " ";
 	}
 	std:: cout << std::endl;
 	return (0);
